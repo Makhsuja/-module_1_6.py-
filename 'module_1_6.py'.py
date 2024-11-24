eb0f73763412893e8e658c4bde0ae980fe0c444c
Makhsuja <makhsuja18@gmail.com>
2024-11-24 18:21:34 +0300

    Update 'module_1_6.py'.py

--- 'module_1_6.py'.py
+++ 'module_1_6.py'.py
@@ -1,19 +1,16 @@
+#Работа со словарями
 my_dict = {'Anton': 2000, 'Sergey': 2002}
 print(my_dict)
-print(my_dict['Anton'])
-my_dict['Max'] = 1999
-print(my_dict)
-print(my_dict['Max'])
+print(my_dict.get('Anton'), my_dict.get("Polina"))
 my_dict.update({'Roma': 2006,
                 'Grisha': 1998})
+del my_dict['Anton']
 print(my_dict)
-del my_dict['Max']
-print(my_dict)
-print(my_dict.get('Max'))
+
+#Работа с множествами
 my_set = {1,2,3,4,1,4,2,'Katya',2.6, 'Katya'}
-print(my_set )
-my_set.update({5,
-               'cow'})
-print(my_set )
-print(my_set.discard(5))
 print(my_set)
+my_set.update({False,
+               'cow'})
+print(my_set.remove(2.6))
+print(my_set )
